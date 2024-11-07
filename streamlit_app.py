09d44520b1a1c87682fe160c376393e45b652cc4
EvelynXu12321 <jianongxu@g.ucla.edu>
2024-11-07 03:14:47 +0000

    Added state scope and FAQ

--- streamlit_app.py
+++ streamlit_app.py
@@ -18,18 +18,26 @@ florida_geojson = {
     ]
 }
 
-data = os.path.join('Florida_county.csv')
-svi_data = pd.read_csv(data)
-svi_data['COUNTY'] = svi_data['COUNTY'].str.replace(r'\s*County', '', regex=True)
+data_county = os.path.join('Florida_county.csv')
+svi_county_data = pd.read_csv(data_county)
+svi_county_data['COUNTY'] = svi_county_data['COUNTY'].str.replace(r'\s*County', '', regex=True)
 
-
+data_state = os.path.join('SVI_2022_US_county.csv')
+svi_state_data = pd.read_csv(data_state)
+svi_state_data['COUNTY'] = svi_state_data['COUNTY'].str.replace(r'\s*County', '', regex=True)
+svi_state_data = svi_state_data[svi_state_data['STATE'] == 'Florida']
 
 # Customize the sidebar
 st.set_page_config(layout="wide")
 # Customize page title
 st.title("Social Vulnerability Index: Map of Florida")
 st.markdown("Data Source: CDC/ATSDR & FGIO")
-st.markdown("Social vulnerablity refers to the demographic and socioeconomic factors that contribute to communities being more adversely affected by public heath emergencies and other external hazards and stressors that cuase disease and injury.")
+st.markdown(
+"""
+    <div style="font-size: 18px;">
+       Social vulnerablity refers to the demographic and socioeconomic factors that contribute to communities being more adversely affected by public heath emergencies and other external hazards and stressors that cuase disease and injury.
+    </div>
+""", unsafe_allow_html=True)
 
 st.write("")  # Adds a blank line
 
@@ -37,17 +45,23 @@ markdown = ("""
 GitHub Repository: <https://github.com/EvelynXu12321/SNOWFLAKE>
 """)
 
-st.sidebar.title("Filters")
 st.sidebar.info(markdown)
-st.sidebar.title("SVI Type")
+st.sidebar.title("Filters")
+
+# Dropdown to select between County and State data
+dataset_option = st.sidebar.selectbox(
+    "Select a Comparison Scope:",
+    ["Country-wide", "State-wide"]
+)
+
 
 # Define display names and map them to actual column names in your dataset
 column_map = {
-    'Socioeconomic Status': 'SPL_THEME1',
-    'Household Characteristics': 'SPL_THEME2',
-    'Racial & Ethnic Minority Status': 'SPL_THEME3',
-    'Housing Type & Transportation': 'SPL_THEME4',
-    'Overall': 'SPL_THEMES'
+    'Socioeconomic Status': 'RPL_THEME1',
+    'Household Characteristics': 'RPL_THEME2',
+    'Racial & Ethnic Minority Status': 'RPL_THEME3',
+    'Housing Type & Transportation': 'RPL_THEME4',
+    'Overall': 'RPL_THEMES'
 }
 
 # Define color mappings for each choropleth type
@@ -61,7 +75,7 @@ color_map = {
 
 # Create a dropdown in the sidebar to select a single layer to display
 selected_option = st.sidebar.selectbox(
-    "Select a Category:",
+    "Select a Theme:",
     list(column_map.keys())
 ).strip()
 
@@ -72,6 +86,11 @@ st.sidebar.write("")  # Add more blank lines if needed
 logo = "https://imgur.com/Nc2lPGT.png"
 st.sidebar.image(logo)
 
+# Set the dataset based on selection
+if dataset_option == "County-level Data":
+    svi_data = svi_county_data
+else:
+    svi_data = svi_state_data
 
 # Map the selected display name to the actual column name and color
 column_name = column_map[selected_option]
@@ -114,11 +133,41 @@ for feature in florida_geojson['features']:
 # folium.LayerControl().add_to(m)
 
 # Display the map in Streamlit
+st.write("")
 st.write(f"Choropleth Map for {selected_option}")
 st_folium(m, width=700, height=500)
 
-#st.caption("SVI provides specific socially and spatially relevant information to help public health officials and local planners better prepare communities to respond to emergency events such as severe weather, floods, disease outbreaks, or chemical exposure.")
+# Additional FAQ content
+st.markdown("""
+    <div style="font-size: 20px;">
+        How do I interpret the CDC/ATSDR SVI ranking for a county?
+    </div>
+""", unsafe_allow_html=True)
 
+st.markdown("""
+    <div style="font-size: 16px;">
+        SVI provides specific socially and spatially relevant information to help public health officials and local planners better prepare communities to respond to emergency events such as severe weather, floods, disease outbreaks, or chemical exposure.
+    </div>
+""", unsafe_allow_html=True)
 
-##TODO
-#state comparoison and country comparison
+st.write("")
+
+st.markdown("""
+    <div style="font-size: 20px;">
+        What is the difference between the U.S.-based and state-based CDC/ATSDR SVI databases?
+    </div>
+""", unsafe_allow_html=True)
+
+st.markdown("""
+    <div style="font-size: 16px;">
+        The U.S.-based CDC/ATSDR SVI database compares the social vulnerability of a county to all counties in the United States. The state-based CDC/ATSDR SVI database compares the social vulnerability of a county solely to counties within a particular state of interest.
+    </div>
+""", unsafe_allow_html=True)
+
+st.write("")
+
+st.markdown("""
+    <div style="font-size: 16px;">
+        For further information, please visit https://www.atsdr.cdc.gov/placeandhealth/svi/data_documentation_download.html.
+    </div>
+""", unsafe_allow_html=True)
